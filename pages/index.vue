6eca6caf19a90eebecbcc91f04e040e68d310ce6
Alicio Alves <alicio.alvesm@gmail.com>
2023-08-23 00:32:28 -0300

    feat: home page

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<main class="overflow-hidden">
+		<div class="flex flex-col gap-24 md:gap-56 xl:gap-80">
+			<Hero />
+			<AboutMe />
+			<Projects />
+			<Articles />
+			<Skills />
+		</div>
+	</main>
+</template>
