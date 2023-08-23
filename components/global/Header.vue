0fd4e517fdc65969230f6c8c87e96ac474f4628b
Alicio Alves <alicio.alvesm@gmail.com>
2023-08-23 00:31:27 -0300

    feat: header

---
+++ components/global/Header.vue
@@ -0,0 +1,23 @@
+<template>
+	<header
+		class="fixed left-0 top-0 z-50 w-full bg-white px-20 py-4 shadow-md dark:bg-[#041F31]"
+	>
+		<div class="flex w-full items-center justify-between">
+			<NuxtLink to="/">Home</NuxtLink>
+
+			<div class="flex w-1/4 items-center justify-between">
+				<NuxtLink to="/">Projects</NuxtLink>
+
+				<NuxtLink to="/">
+					<button class="btn btn-primary">Contact</button>
+				</NuxtLink>
+
+				<div>
+					<ThemeToggler />
+				</div>
+			</div>
+		</div>
+	</header>
+</template>
+
+<script setup lang="ts"></script>
