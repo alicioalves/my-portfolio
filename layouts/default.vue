684943f577ae5adc5b86c5046928565822342411
Alicio Alves <alicio.alvesm@gmail.com>
2023-08-23 00:31:08 -0300

    chore: default layout setup

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+	<div
+		class="flex h-full min-h-screen w-full flex-col bg-white px-32 text-black dark:bg-[#041F31] dark:text-white"
+	>
+		<Header />
+
+		<main class="mx-auto w-full">
+			<slot />
+		</main>
+
+		<ScrollToTop />
+		<Footer />
+	</div>
+</template>
